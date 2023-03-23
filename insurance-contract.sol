a78df676fe23287554a8a0d32962a6ba44c91874
Indy <in.spaywer@gmail.com>
2023-03-23 21:41:24 +0700

    fix name insurance

--- insurance-contract.sol
+++ insurance-contract.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-contract DRS_DATA_STORE {
+contract INSURANCE_CONTRACT {
     struct ContractData {
         address user;
         address company;
         uint256 contractValue;
+        string contractData;
         string start;
         string expire;
     }
